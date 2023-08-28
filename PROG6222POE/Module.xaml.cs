326ba42353e260e13adfe152a8473d18cb4ad943
lab_services_student <lab_services_student@lab000000>
2023-08-28 09:43:48 +0000

    Add project files.

---
+++ PROG6222POE/Module.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PROG6222POE
+{
+    /// <summary>
+    /// Interaction logic for Module.xaml
+    /// </summary>
+    public partial class Module : Window
+    {
+        public Module()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
